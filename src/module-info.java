db7b9bd59cf780f085b401d0ec73d41d2108ed06
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-07-28 21:55:29 +0300

    LD-21 #comment solve dependencies

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Vrinceanu Vladut
+ * Date: 28-07-2021
+ * Time: 21:52
+ * OS: Windows
+ */
+
+module overtime {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires poi;
+    requires java.desktop;
+
+    opens com.overtime;
+}
